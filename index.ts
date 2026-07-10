b74eba2b5090c32ff11394c2cd26e03034774f2f
SalzDevs <a49000@alunos.isel.pt>
2026-07-10 14:54:10 +0100

    html parser and visit function of the tree

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import * as parse5 from "parse5"
+
+function visit(node: any, depth = 0) {
+    console.log(
+        " ".repeat(depth * 2),
+        node.nodeName,
+        node.value ?? ""
+    );
+
+    if (node.childNodes) {
+        for (const child of node.childNodes) {
+            visit(child, depth + 1);
+        }
+    }
+}
+
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Hello!</h1>
+    <p>This is <strong>OpenTUI</strong>.</p>
+  </body>
+</html>
+`;
+
+const document = parse5.parse(html);
+visit(document);
